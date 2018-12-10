ac0b964c15fef97f91cd7fdfcc8bb3d34bfed9f8
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-10 21:57:15 +0100

    Use JATS backend in all tests

--- test/asciidoctor/jats_test.rb
+++ test/asciidoctor/jats_test.rb
@@ -8,12 +8,12 @@ class Asciidoctor::JatsTest < Minitest::Test
   end
 
   def test_that_calling_asciidoctor_works
-    result = Asciidoctor.convert 'This is a paragraph.'
+    result = Asciidoctor.convert 'This is a paragraph.', backend: :jats
     refute_empty result, 'Expected Asciidoctor to generate output, but was empty'
   end
 
   def test_that_empty_document_generates_xml_header
-    xml = Asciidoctor.convert('')
+    xml = Asciidoctor.convert '', backend: :jats
     actual = Nokogiri::XML(xml).to_s
     expected = "<?xml version=\"1.0\"?>\n"
     assert_equal actual, expected, 'Expected XML header'
