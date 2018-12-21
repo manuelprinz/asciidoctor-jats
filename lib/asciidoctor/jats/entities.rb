4f3e554ecf0922638272aa9e6245d94a28f2a325
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-21 22:25:28 +0100

    Add strong class

--- lib/asciidoctor/jats/entities.rb
+++ lib/asciidoctor/jats/entities.rb
@@ -27,5 +27,21 @@ module Asciidoctor
         end
       end
     end
+
+    class Strong
+      attr_reader :text
+
+      def initialize(node)
+        @text = node.text
+      end
+
+      def to_s
+        if text.empty?
+          ''
+        else
+          "<bold>#{text}</bold>"
+        end
+      end
+    end
   end
 end
