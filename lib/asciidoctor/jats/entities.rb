5c95479a748b8bda40c07713f83805891469441c
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-21 18:53:51 +0100

    Add emphasis class

--- lib/asciidoctor/jats/entities.rb
+++ lib/asciidoctor/jats/entities.rb
@@ -11,5 +11,21 @@ module Asciidoctor
         "<p>#{content}</p>"
       end
     end
+
+    class Emphasis
+      attr_reader :text
+
+      def initialize(node)
+        @text = node.text
+      end
+
+      def to_s
+        if text.empty?
+          ''
+        else
+          "<italic>#{text}</italic>"
+        end
+      end
+    end
   end
 end
