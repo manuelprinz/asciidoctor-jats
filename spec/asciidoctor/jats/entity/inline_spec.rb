e111d44b8948e05d8d859d7f5db30989fdc5613f
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-22 18:21:42 +0100

    Add spec for inline class

---
+++ spec/asciidoctor/jats/entity/inline_spec.rb
@@ -0,0 +1,42 @@
+require 'rspec'
+
+require 'asciidoctor'
+require 'asciidoctor-jats'
+
+RSpec.describe Asciidoctor::JATS::Entity::Inline do
+  context '.for' do
+    it 'should return +Emphasis+ class for emphasized text' do
+      actual = described_class.for(node_of_type(:emphasis))
+      expect(actual).to be_a(Asciidoctor::JATS::Entity::Emphasis)
+    end
+
+    it 'should return +Strong+ class for strong text' do
+      actual = described_class.for(node_of_type(:strong))
+      expect(actual).to be_a(Asciidoctor::JATS::Entity::Strong)
+    end
+
+    it 'should return +Monospace+ class for monospaced text' do
+      actual = described_class.for(node_of_type(:monospaced))
+      expect(actual).to be_a(Asciidoctor::JATS::Entity::Monospace)
+    end
+
+    it 'should return +Superscript+ class for superscript text' do
+      actual = described_class.for(node_of_type(:superscript))
+      expect(actual).to be_a(Asciidoctor::JATS::Entity::Superscript)
+    end
+
+    it 'should return +Subscript+ class for subscript text' do
+      actual = described_class.for(node_of_type(:subscript))
+      expect(actual).to be_a(Asciidoctor::JATS::Entity::Subscript)
+    end
+
+    it 'should return +NoEntity+ class for unknown text' do
+      actual = described_class.for(node_of_type(:unsupported_tag))
+      expect(actual).to be_a(Asciidoctor::JATS::Entity::NoEntity)
+    end
+  end
+
+  def node_of_type(type)
+    Asciidoctor::Inline.new(nil, :quoted, 'irrelevant', type: type)
+  end
+end
