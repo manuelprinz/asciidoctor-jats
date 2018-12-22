ac0c3f54b91f2d5e402a201ef663844ff84b8463
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-22 16:01:16 +0100

    Fix wrong example description in spec

--- spec/asciidoctor/jats/entity/strong_spec.rb
+++ spec/asciidoctor/jats/entity/strong_spec.rb
@@ -5,13 +5,13 @@ require 'asciidoctor-jats'
 
 RSpec.describe Asciidoctor::JATS::Entity::Strong do
   context '#to_s' do
-    it 'should wrap a word in <italic> tags' do
+    it 'should wrap a word in <bold> tags' do
       actual = build_strong('Example').to_s
       expected = '<bold>Example</bold>'
       expect(actual).to eq(expected)
     end
 
-    it 'should wrap a phrase in <italic> tags' do
+    it 'should wrap a phrase in <bold> tags' do
       actual = build_strong('This is an example.').to_s
       expected = '<bold>This is an example.</bold>'
       expect(actual).to eq(expected)
