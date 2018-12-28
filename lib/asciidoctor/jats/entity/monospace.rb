f345c1afccfa2f7d9e8eba3c4ca0c06267288e7b
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-28 22:14:29 +0100

    Add documentation for monospace text

--- lib/asciidoctor/jats/entity/monospace.rb
+++ lib/asciidoctor/jats/entity/monospace.rb
@@ -1,15 +1,20 @@
 module Asciidoctor
   module JATS
     module Entity
+      # Monospace text. Wraps some content into +monospace+ tags.
       class Monospace < SingleTag
+        # Create a new [Monospace].
+        # @param node [Asciidoctor::Inline] an inline block
         def initialize(node)
           @text = node.text
         end
 
+        # @return [String] the tag +<monospace>+
         def tag_name
           'monospace'
         end
 
+        # @return [] the text of the block
         def content
           @text
         end
