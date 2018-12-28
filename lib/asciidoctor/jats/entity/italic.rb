d175fb8396f5ed024075b7be620744f6ebb3ff19
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-28 22:05:28 +0100

    Add documentation for italic text

--- lib/asciidoctor/jats/entity/italic.rb
+++ lib/asciidoctor/jats/entity/italic.rb
@@ -1,15 +1,20 @@
 module Asciidoctor
   module JATS
     module Entity
+      # Italic text. Wraps some content into +italic+ tags.
       class Italic < SingleTag
+        # Create a new [Italic].
+        # @param node [Asciidoctor::Inline] an inline block
         def initialize(node)
           @text = node.text
         end
 
+        # @return [String] the tag +<italic>+
         def tag_name
           'italic'
         end
 
+        # @return [] the text of the block
         def content
           @text
         end
