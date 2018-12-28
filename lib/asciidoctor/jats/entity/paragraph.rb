17abb39a252bb0845d33f79b64c7b471f4e941be
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-28 21:59:45 +0100

    Add documentation for paragraphs

--- lib/asciidoctor/jats/entity/paragraph.rb
+++ lib/asciidoctor/jats/entity/paragraph.rb
@@ -1,13 +1,18 @@
 module Asciidoctor
   module JATS
     module Entity
+      # A paragraph. Wraps some content into +<p>+ tags.
       class Paragraph < SingleTag
+        # Create a new [Paragraph].
+        # @param content [] the content to be wrapped.
         def initialize(content:)
           @content = content
         end
 
+        # @return [] the content of the paragraph
         attr_reader :content
 
+        # @return [String] the tag +<p>+
         def tag_name
           'p'
         end
