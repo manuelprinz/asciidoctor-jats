58d312bdd8ba64baeb173f5e93f7a25648b6800d
Manuel Prinz <manuel@pinguinkiste.de>
2018-12-28 22:09:19 +0100

    Add documentation for bold text

--- lib/asciidoctor/jats/entity/bold.rb
+++ lib/asciidoctor/jats/entity/bold.rb
@@ -1,15 +1,20 @@
 module Asciidoctor
   module JATS
     module Entity
+      # Bold text. Wraps some content into +bold+ tags.
       class Bold < SingleTag
+        # Create a new [Bold].
+        # @param node [Asciidoctor::Inline] an inline block
         def initialize(node)
           @text = node.text
         end
 
+        # @return [String] the tag +<bold>+
         def tag_name
           'bold'
         end
 
+        # @return [] the text of the block
         def content
           @text
         end
